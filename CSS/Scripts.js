1aac2a750abf43a808cf2e72e183aedab6b7bf6a
Arthur65524 <arthur.meinung@gmail.com>
2025-05-18 12:34:16 -0700

    About Page Update

--- CSS/Scripts.js
+++ CSS/Scripts.js
@@ -7,3 +7,53 @@ $(document).ready(function(){
         }
     })
 })
+
+// About Page
+$(document).ready(function() {
+    //Open Modal
+    $('.open-modal').click(function() {
+        var targetModal = $(this).data("target");
+        $(targetModal).show();
+    })
+
+    //Close Modal
+    $('.close').click(function() {
+        $(this).closest('.modal').hide();
+    })
+})
+$(document).ready(function(){
+    $('.carousel').slick({
+        dots: true,
+        arrows: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        responsive: [
+            {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+            },
+            {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+            }
+        ]
+    });
+})
+$(document).ready(function() {
+    //Open Modal
+    $('.open-modal2').click(function() {
+        var targetModal = $(this).data("target");
+        $(targetModal).show();
+    })
+
+    //Close Modal
+    $('.close').click(function() {
+        $(this).closest('.modal').hide();
+    })
+})
