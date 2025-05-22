107dff5e2d3f09d387f61a9959e65d7f2ace600c
Arthur65524 <arthur.meinung@gmail.com>
2025-05-22 08:37:07 -0700

    horses for sale page

--- CSS/Scripts.js
+++ CSS/Scripts.js
@@ -101,3 +101,30 @@ $(document).ready(function() {
         }
     });
 })
+
+// Horses for sale page
+$(document).ready(function() {
+    //Open Modal
+    $('.open-modal4').click(function() {
+        var targetModal = $(this).data("target");
+        $(targetModal).show();
+    })
+
+    //Close Modal
+    $('.close').click(function() {
+        $(this).closest('.modal4').hide();
+    })
+})
+
+$(document).ready(function() {
+    //Open Modal
+    $('.open-modal3').click(function() {
+        var targetModal = $(this).data("target");
+        $(targetModal).show();
+    })
+
+    //Close Modal
+    $('.close').click(function() {
+        $(this).closest('.modal').hide();
+    })
+})
